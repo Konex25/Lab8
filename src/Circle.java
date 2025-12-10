9ac9d07b9d72fcc672e7049a23171d05dceca989
Konex25 <rapacz.konrad@gmail.com>
2025-12-10 14:07:44 +0100

    Circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("Radius cannot be negative.");
+        }
+        this.radius = radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 }
