23aa433afcc4b22a440592a820dbabbf49a3a00f
Konex25 <rapacz.konrad@gmail.com>
2025-12-10 14:09:23 +0100

    Sphere class

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Sphere {
+
+    private double radius;
+
+    public Sphere(double radius) {
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("Radius cannot be negative.");
+        }
+        this.radius = radius;
+    }
+
+    public double getSurfaceArea() {
+        return 4 * Math.PI * radius * radius;
+    }
+
+    public double getVolume() {
+        return (4.0 / 3.0) * Math.PI * radius * radius * radius;
+    }
 }
